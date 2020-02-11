62ac8a8cffbfa24e8f9605cafdd2c77f8a6cd81b
kumekawakanau <kumekawakanau@yahoo.co.jp>
2020-02-11 17:49:40 +0900

    注釈を追記

--- main.cpp
+++ main.cpp
@@ -9,17 +9,21 @@ using namespace std;
 int main(void) {
 	while (true) {
 		string usiIn;
+		//GUIから受信
 		getline(cin, usiIn);
 
 		vector<string> tokens;
 		stringstream sts(usiIn);
 		string token;
+		//GUIから受信した文字列を空白で区切る
 		while (getline(sts, token, ' ')) {
 			tokens.push_back(token);
 		}
+		//何も受信できなかったら再び待つ
 		if (tokens.empty()) {
 			continue;
 		}
+		//受信内容によって動作変更
 		if (tokens[0] == "usi") {
 			cout << "id name kumesan'sShogi" << endl;
 			cout << "id author kumesan" << endl;
